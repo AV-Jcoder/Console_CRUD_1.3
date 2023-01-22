2f1c6a7274cd5880eda6eaddd558c4059912c959
aleksej <aleksej.afonin@gmail.com>
2023-01-22 18:10:40 +0700

    define private methods and fields in GsonSpeciality class

--- src/main/java/com/afoninav/repository/gson/GsonSpecialityRepositoryImpl.java
+++ src/main/java/com/afoninav/repository/gson/GsonSpecialityRepositoryImpl.java
@@ -1,11 +1,32 @@
 package com.afoninav.repository.gson;
 
+import com.afoninav.model.Speciality;
 import com.afoninav.repository.SpecialityRepository;
+import com.google.gson.Gson;
 
+import java.util.LinkedList;
 import java.util.List;
 
 // TODO: write the interface's method body
 public class GsonSpecialityRepositoryImpl implements SpecialityRepository {
+
+    private static final String FILE_PATH = "src/main/resources/skills.json";
+    private static final Gson GSON = new Gson();
+
+    private List<Speciality> readAllSpecialitiesFromDisc() {
+        // TODO: impl method body
+        return new LinkedList<>();
+    }
+
+    private void writeAllSpecialitiesToDisc() {
+        // TODO: impl method body
+    }
+
+    private int generateId() {
+        // TODO imple method body
+        return 0;
+    }
+
     @Override
     public Object create(Object o) {
         return null;
