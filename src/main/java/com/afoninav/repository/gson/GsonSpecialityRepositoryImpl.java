4b5ad1fdd41ee115bfb45b6f2cfb4a8aa20536d3
aleksej <aleksej.afonin@gmail.com>
2023-01-22 17:51:52 +0700

    add GsonSpecialityRepositoryImpl

---
+++ src/main/java/com/afoninav/repository/gson/GsonSpecialityRepositoryImpl.java
@@ -0,0 +1,33 @@
+package com.afoninav.repository.gson;
+
+import com.afoninav.repository.SpecialityRepository;
+
+import java.util.List;
+
+// TODO: write the interface's method body
+public class GsonSpecialityRepositoryImpl implements SpecialityRepository {
+    @Override
+    public Object create(Object o) {
+        return null;
+    }
+
+    @Override
+    public Object readById(Object o) {
+        return null;
+    }
+
+    @Override
+    public Object update(Object o) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteById(Object o) {
+        return false;
+    }
+
+    @Override
+    public List readAll() {
+        return null;
+    }
+}
