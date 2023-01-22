1c11c73d87a860718c86be282a7381f6af105c0e
aleksej <aleksej.afonin@gmail.com>
2023-01-22 17:49:45 +0700

    add class GsonSkillRepositoryImpl

---
+++ src/main/java/com/afoninav/repository/gson/GsonSkillRepositoryImpl.java
@@ -0,0 +1,34 @@
+package com.afoninav.repository.gson;
+
+import com.afoninav.model.Skill;
+import com.afoninav.repository.SkillRepository;
+
+import java.util.List;
+
+// TODO: write interface's method body
+public class GsonSkillRepositoryImpl implements SkillRepository {
+    @Override
+    public Skill create(Skill skill) {
+        return null;
+    }
+
+    @Override
+    public Skill readById(Integer integer) {
+        return null;
+    }
+
+    @Override
+    public Skill update(Skill skill) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteById(Integer integer) {
+        return false;
+    }
+
+    @Override
+    public List<Skill> readAll() {
+        return null;
+    }
+}
