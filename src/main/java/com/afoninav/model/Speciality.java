77c4ab55dfbc6f573ab830c3e978733a8b141958
aleksej <aleksej.afonin@gmail.com>
2023-01-22 20:14:03 +0700

    Fix Speciality pojo

--- src/main/java/com/afoninav/model/Speciality.java
+++ src/main/java/com/afoninav/model/Speciality.java
@@ -10,6 +10,22 @@ public class Speciality {
         this.title = title;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     @Override
     public String toString() {
         return title;
