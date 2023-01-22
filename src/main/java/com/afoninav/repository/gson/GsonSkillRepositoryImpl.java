140ebe6bd21f951ded1b18e2e5481634354a25b8
aleksej <aleksej.afonin@gmail.com>
2023-01-22 22:18:10 +0700

    add body to private method in GsonSkill class

--- src/main/java/com/afoninav/repository/gson/GsonSkillRepositoryImpl.java
+++ src/main/java/com/afoninav/repository/gson/GsonSkillRepositoryImpl.java
@@ -3,9 +3,15 @@ package com.afoninav.repository.gson;
 import com.afoninav.model.Skill;
 import com.afoninav.repository.SkillRepository;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 // TODO: write interface's method body
 public class GsonSkillRepositoryImpl implements SkillRepository {
@@ -14,18 +20,32 @@ public class GsonSkillRepositoryImpl implements SkillRepository {
     private static final Gson GSON = new Gson();
 
     private List<Skill> readAllSkillsFromDisc() {
-        // TODO: implement method body
-        return new LinkedList<>();
+        LinkedList<Skill> list = null;
+        try (FileInputStream in = new FileInputStream(FILE_PATH)) {
+            String json = new String(in.readAllBytes());
+            TypeToken<LinkedList<Skill>> token = new TypeToken<>(){};
+            list = GSON.fromJson(json, token);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Objects.nonNull(list) ? list : new LinkedList<>();
     }
 
-    private void writeAllSkillsToDisc() {
-        // TODO: implement method body
-
+    private void writeAllSkillsToDisc(List<Skill> list) {
+        try (FileOutputStream out = new FileOutputStream(FILE_PATH)) {
+            String json = GSON.toJson(list);
+            out.write(json.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private int generateSkillId() {
-        // TODO: implement method body
-        return 0;
+    private int generateSkillId(List<Skill> list) {
+        Skill  skill = list
+                .stream()
+                .max(Comparator.comparing(Skill::getId))
+                .orElse(null);
+        return Objects.nonNull(skill) ? skill.getId()+1 : 1;
     }
 
     @Override
